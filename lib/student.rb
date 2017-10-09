201043018db9f05a20e08a7bc0f53f7076a9227f
Chris Rudolph <chrisjames.r@gmail.com>
2017-10-09 14:23:10 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -56,4 +56,8 @@ class Student
     DB[:conn].execute(sql, self.name, self.grade, self.id)
   end
 
+  def self.new_from_db(row)
+    Student.new(row[0], row[1], row[2])
+  end
+
 end  #  End of Class
